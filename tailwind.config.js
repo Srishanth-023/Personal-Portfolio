cd64f6328969af4bbdc20f27e06afd4dd59d2121
SRISHANTH P M <srishanth232007@gmail.com>
2025-08-19 14:02:56 +0530

    configs of postcss & tailwinds updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -8,25 +8,24 @@ export default {
     extend: {
       colors: {
         gray: {
-          200: "#D5DAE1"
+          200: "#D5DAE1",
         },
         black: {
           DEFAULT: "#000",
-          500: "#1D2235"
+          500: "#1D2235",
         },
         blue: {
-          500: "#2b77e7"
-        }
+          500: "#2b77e7",
+        },
       },
       fontFamily: {
         worksans: ["Work Sans", "sans-serif"],
-        poppins: ['Poppins', "sans-serif"]
+        poppins: ["Poppins", "sans-serif"],
       },
       boxShadow: {
-        card: '0px 1px 2px 0px rgba(0, 0, 0, 0.05)'
-      }
+        card: "0px 1px 2px 0px rgba(0, 0, 0, 0.05)",
+      },
     },
   },
   plugins: [],
-}
-
+};
